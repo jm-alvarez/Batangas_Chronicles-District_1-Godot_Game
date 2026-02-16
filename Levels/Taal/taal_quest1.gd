38142237a29f8b23188156b7ac4b27caf5b8dac1
jm-alvarez <alvarezjohnmark118@gmail.com>
2026-02-17 00:16:42 +0800

    Taal Quest2 Step 1 Bug Fixes

--- Levels/Taal/taal_quest1.gd
+++ Levels/Taal/taal_quest1.gd
@@ -22,23 +22,26 @@ func _on_dialog_interaction_finished() -> void:
 
 
 func _on_npc_man_1_trigger_player_entered() -> void:
-	%taal_quest1_advance.advance_quest()
+	
 	pass # Replace with function body.
 
 
 func _on_ermitanyo_1_interaction_finished() -> void:
 	%taal_quest_Show.advance_quest()
 	await get_tree().process_frame
-	%Ermitanyo1.queue_free()
+	%ermitanyo1_interaction.queue_free()
+	#await get_tree().process_frame
+	#%Ermitanyo1.queue_free()
 	pass # Replace with function body.
 
 
 func _on_npc_man_1_interaction_finished() -> void:
-	%Spaniard_Camp.queue_free()
-	%Spaniard_NPC1.queue_free()
+	#%Spaniard_Camp.queue_free()
+	#%Spaniard_NPC1.queue_free()
+	%taal_quest1_advance.advance_quest()
 	%Spaniard_enemy_Q2.process_mode = Node.PROCESS_MODE_INHERIT
 	%Spaniard_enemy_Q2.visible = true
-	%npc_man1.queue_free()
+	#%npc_man1.queue_free()
 	pass # Replace with function body.
 
 func count_spaniards() :
@@ -47,7 +50,7 @@ func count_spaniards() :
 		print("spaniard count on taal: ", cnt)
 		if cnt == 1:
 			print("All spaniards defeated.")
-			$taal_Q2_advance.advance_quest()
+			%taal_Q1_QS2_advance.advance_quest()
 			await get_tree().process_frame
 			%Spaniard_enemy_Q2.queue_free()
 		
@@ -65,6 +68,7 @@ func _on_earthquake_1_player_entered() -> void:
 
 
 func _on_yes_selected() -> void:
-	%taal_q1_qs3_advance.advance_quest()
 	%taal_q2_show_advance2.advance_quest()
+	await get_tree().process_frame
+	%taal_q1_qs3_advance.advance_quest()
 	pass # Replace with function body.
