2c17a3c55abf8cde6c31ecd9b6c41248b058dbcb
jm-alvarez <alvarezjohnmark118@gmail.com>
2026-02-17 16:22:12 +0800

    Taal Quest 2 Step 2 Completed With Cutscenes

--- Levels/Taal/taal_quest_2.gd
+++ Levels/Taal/taal_quest_2.gd
@@ -9,3 +9,19 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_lc_cutscene_2_finished() -> void:
+	
+	pass # Replace with function body.
+
+
+func _on_trigger_strong_eq_player_entered() -> void:
+	PlayerManager.shake_camera(3)
+	await get_tree().process_frame
+	pass # Replace with function body.
+
+
+func _on_lc_cutscene_3_finished() -> void:
+	%taal_q2_qs2_advance.advance_quest()
+	pass # Replace with function body.
