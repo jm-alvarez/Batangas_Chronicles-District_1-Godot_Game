5dc2c15e2e428e0fd58b3914f11c8a0dd3ae452f
jm-alvarez <alvarezjohnmark118@gmail.com>
2026-03-16 23:51:42 +0800

    Export Preset + Hide Control Buttons when Detect Joypads Feature

--- GUI/ControlHUD/touch_control.gd
+++ GUI/ControlHUD/touch_control.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
 
-func _input(event):
-	if event.is_action_pressed("inventory"):
-		$Inventory.visible = !$Inventory.visible
+#func _input(event):
+	#if event.is_action_pressed("inventory"):
+		#$Inventory.visible = !$Inventory.visible
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if Input.get_connected_joypads():
+		print("Joypads Connected")
+		%r_btn.visible = false
+		%pause_node.visible = false
+		%"d-pad_node".visible = false
 	pass # Replace with function body.
 
 
